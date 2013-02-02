4b75183f9d2d17a85dab768cd5cc36e4864f6a77
Alex Ames <Alexander.Ames@gmail.com>
2013-02-02 00:03:36 -0800

    Moved midifileeventproducer code into midifileeventproducer.cpp, since it's not platform specific

---
+++ src/midiutils/midifileeventproducer.cpp
@@ -0,0 +1,56 @@
+#include "midiutils.hpp"
+
+#include <limits>
+
+using namespace std;
+
+namespace midi
+{
+
+MidiFileEventProducer::MidiFileEventProducer(MidiFile& midi)
+	: m_midi(&midi)
+	, m_lastEventTime(0)
+	, m_indicies(midi.tracks.size())
+	, m_absoluteTimes(midi.tracks.size())
+{
+}
+
+static const Event* getNextEventInTrack(const Track& track, unsigned int& index, unsigned int& absoluteTime)
+{
+	while (index < track.events.size())
+	{
+		const Event* event = &track.events[index++];
+		absoluteTime += event->timeDelta;
+			
+		if (event->command != Event::Meta)
+		{
+			return event;
+		}
+	}
+	return nullptr;
+}
+
+const Event* MidiFileEventProducer::getNextEvent(unsigned int& absoluteTime)
+{
+	const Event* bestEvent = nullptr;
+	unsigned int bestTime = numeric_limits<unsigned int>::max();
+	for (unsigned int i = 0; i < m_midi->tracks.size(); i++)
+	{
+		const Track& track = m_midi->tracks[i];
+		const Event* event = getNextEventInTrack(track, m_indicies[i], m_absoluteTimes[i]);
+		if (event && m_absoluteTimes[i] <= bestTime)
+		{
+			bestEvent = event;
+			bestTime = m_absoluteTimes[i];
+		}
+	}
+	absoluteTime = bestTime;
+	return bestEvent;
+}
+
+unsigned int MidiFileEventProducer::getTicksPerBeat()
+{
+	return m_midi->ticks;
+}
+
+}// namespace midi
--- src/midiutils/win32/midistreamimpl.cpp
+++ src/midiutils/win32/midistreamimpl.cpp
@@ -5,7 +5,6 @@
 #include "mmsyscom.h"
 
 #include <vector>
-#include <limits>
 
 using namespace std;
 
@@ -178,54 +177,6 @@ int MidiEventBuffer::getBytesFilled()
 
 ////////////////////////////////////////////////////////////////////////////////
 
-MidiFileEventProducer::MidiFileEventProducer(MidiFile& midi)
-	: m_midi(&midi)
-	, m_lastEventTime(0)
-	, m_indicies(midi.tracks.size())
-	, m_absoluteTimes(midi.tracks.size())
-{
-}
-
-static const Event* getNextEventInTrack(const Track& track, unsigned int& index, unsigned int& absoluteTime)
-{
-	while (index < track.events.size())
-	{
-		const Event* event = &track.events[index++];
-		absoluteTime += event->timeDelta;
-			
-		if (event->command != Event::Meta)
-		{
-			return event;
-		}
-	}
-	return nullptr;
-}
-
-const Event* MidiFileEventProducer::getNextEvent(unsigned int& absoluteTime)
-{
-	const Event* bestEvent = nullptr;
-	unsigned int bestTime = numeric_limits<unsigned int>::max();
-	for (unsigned int i = 0; i < m_midi->tracks.size(); i++)
-	{
-		const Track& track = m_midi->tracks[i];
-		const Event* event = getNextEventInTrack(track, m_indicies[i], m_absoluteTimes[i]);
-		if (event && m_absoluteTimes[i] <= bestTime)
-		{
-			bestEvent = event;
-			bestTime = m_absoluteTimes[i];
-		}
-	}
-	absoluteTime = bestTime;
-	return bestEvent;
-}
-
-unsigned int MidiFileEventProducer::getTicksPerBeat()
-{
-	return m_midi->ticks;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
 static void CALLBACK midiStreamEventCallback(HMIDIOUT handle, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
 {
 	MidiStreamImpl* stream = (MidiStreamImpl*)dwInstance;	
@@ -240,6 +191,7 @@ static void CALLBACK midiStreamEventCallback(HMIDIOUT handle, UINT uMsg, DWORD d
 
 	case MOM_OPEN:
 		break;
+
 	case MOM_CLOSE:
 		break;
 	}
